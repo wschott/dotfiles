c174089b0249c30aa825e90332874d8edfc7c89f
Waldemar Schott <code@waldemarschott.com>
2012-01-18 17:07:01 +0100

    Add fdj() to search django sources.

--- aliases/python.sh
+++ aliases/python.sh
@@ -16,3 +16,14 @@ dj() {
 }
 complete -F _django_completion -o default dj
 alias djr='python manage.py runserver'
+
+# search Djangos source files ------------------------------------------------
+# /via https://code.djangoproject.com/wiki/CookBookScriptsAliases
+if [[ $(uname) == 'Darwin' ]]; then
+    DJROOT=/Library/Python/2.7/site-packages/django
+else
+    DJROOT=/usr/lib/python2.7/site-packages/django
+fi
+fdj() {
+    grep -r --include="*.py" --include="*.html" $* $DJROOT
+}
