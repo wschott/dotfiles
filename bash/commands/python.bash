09bd645e5c2815224cf0c7ccb9093593fe59587f
Waldemar Schott <info@waldemarschott.com>
2012-01-26 17:01:32 +0100

    Change virtualenvwrapper.sh existance test

--- bash/commands/python.bash
+++ bash/commands/python.bash
@@ -5,7 +5,7 @@ alias py='python'
 alias ipy='ipython'
 
 # virtualenvwrapper support --------------------------------------------------
-if [[ ! -z $(which virtualenvwrapper.sh) ]]; then
+if which virtualenvwrapper.sh >/dev/null 2>&1; then
     export WORK_ON='~/.virtualenvs'
     export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
     source $(which virtualenvwrapper.sh)
